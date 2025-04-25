771019c2b463dea92dfa9fc82e1d8c29b3e41eda
JSGONZc13 <jairo99gc@gmail.com>
2025-04-24 19:18:45 -0500

    feat: input con variantes

--- lib/Global/presentation/components/inputs.dart
+++ lib/Global/presentation/components/inputs.dart
@@ -19,19 +19,22 @@ final textFieldDecoration = InputDecoration(
 );
 
 class CustomInputField extends StatelessWidget {
+  final String? variant;
   final TextEditingController controller;
   final void Function(String)? onSubmitted;
   const CustomInputField({
     super.key,
     required this.controller,
     this.onSubmitted,
+    this.variant = 'default',
   });
   @override
   Widget build(BuildContext context) {
     return TextField(
       controller: controller,
       onSubmitted: onSubmitted,
-      decoration: textFieldDecoration,
+      decoration:
+          variant == 'default' ? textFieldDecoration : null,
       style: pFont,
     );
   }
